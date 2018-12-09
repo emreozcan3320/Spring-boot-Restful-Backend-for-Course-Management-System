f8c234fee095b5a39127cc6e3d14cd18c8ef5c66
emreozcan3320 <emreozcan3320@gmail.com>
2018-12-09 17:36:31 +0300

    UPDATE and DELETE end-points were added

--- src/main/java/com/kadiremreozcan/springbootstarter/controller/TopicController.java
+++ src/main/java/com/kadiremreozcan/springbootstarter/controller/TopicController.java
@@ -30,7 +30,16 @@ public class TopicController {
 	
 	@RequestMapping(method=RequestMethod.POST,value="/topics")
 	public void addTopic(@RequestBody Topic topic) {
-		topicService.addTopic(topic);
-		
+		topicService.addTopic(topic);		
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,value="/topics/{id}")
+	public void UpdateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id,topic);		
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE,value="/topics/{id}")
+	public void UpdateTopic( @PathVariable String id) {
+		topicService.deleteTopic(id);		
 	}
 }
